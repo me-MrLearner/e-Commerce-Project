6e4208fe504fb895b386c0f7280aff2fe2292f01
Mr Learner <mr.learnerspace@gmail.com>
2024-11-09 20:15:00 +0600

    add a dart file

---
+++ myCode.dart
@@ -0,0 +1,10 @@
+class Mycode {
+  Mycode(String fName, String lName) {
+    String name = fName + ' ' + lName;
+    print('Hello, $name');
+  }
+}
+
+void main() {
+  Mycode obj = Mycode('Mr', 'Shohanur');
+}
